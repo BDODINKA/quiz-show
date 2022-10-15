812a5187619d5d3def29a421d60f940d8ccf61d5
DMITRY <bdodinka1992@gmail.com>
2022-10-15 20:13:02 +0300

    feat:add Store types

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,12 +1,29 @@
-import { combineReducers, createStore, applyMiddleware } from 'redux'
-import thunk from 'redux-thunk'
+import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
+import { applyMiddleware, combineReducers, createStore } from 'redux'
+import thunk, { ThunkAction, ThunkDispatch } from 'redux-thunk'
 
-const rootReducer = combineReducers({
-  // auth:
+export type AppRootStateType = ReturnType<typeof rootReducer>
+
+export const rootReducer = combineReducers({
+  // auth: loginReducer,
   // profile:
 })
 
-const store = createStore(rootReducer, applyMiddleware(thunk))
+export type ActionsType = any
+
+export const store = createStore(rootReducer, applyMiddleware(thunk))
+
+export type RootStateType = ReturnType<typeof store.getState>
+export type AppDispatch = ThunkDispatch<RootStateType, undefined, ActionsType>
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootStateType,
+  unknown,
+  ActionsType
+>
+
+export const useAppDispatch = () => useDispatch<AppDispatch>()
+export const useAppSelector: TypedUseSelectorHook<RootStateType> = useSelector
 
 //@ts-ignore
 window.store = store
