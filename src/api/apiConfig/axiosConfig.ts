a76c6e8bdbb2c255726e69cb0a61f38c0e7aa61f
DmitryPatskalev <dimanord@yandex.ru>
2022-10-24 10:21:37 +0300

    create_table_branch

--- src/api/apiConfig/axiosConfig.ts
+++ src/api/apiConfig/axiosConfig.ts
@@ -1,9 +1,10 @@
-import axios from 'axios'
+import axios from "axios";
 
 export const axiosInstance = axios.create({
   baseURL:
-    process.env.NODE_ENV === 'development'
-      ? 'http://localhost:7542/2.0/'
-      : 'https://neko-back.herokuapp.com/2.0/',
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:7542/2.0/"
+      : "https://neko-back.herokuapp.com/2.0/",
   withCredentials: true,
-})
+});
+//axios config
