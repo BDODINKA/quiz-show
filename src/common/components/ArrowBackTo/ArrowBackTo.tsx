290ad07218dd998f01f7fe5966b3c34ed8214688
Cherniakov_Pavel <74683152+Pavel5284@users.noreply.github.com>
2022-12-02 16:28:34 +0200

    style: ArrowBackTo.tsx

--- src/common/components/ArrowBackTo/ArrowBackTo.tsx
+++ src/common/components/ArrowBackTo/ArrowBackTo.tsx
@@ -2,13 +2,17 @@ import React from 'react'
 
 import { NavLink } from 'react-router-dom'
 
-import style from '../../../features/profile/profile.module.css'
+import { ReactComponent as ArrowBack } from '../../../assets/svg/arrowProfile.svg'
 import { PATH } from '../../routes/const-routes'
+
+import style from './ArrowBackTo.module.scss'
 
 export const ArrowBackTo = () => {
   return (
-    <div className={style.backBlock}>
-      <NavLink to={PATH.PACK_CARDS_PAGE} className={style.arrow} />
+    <div className={style.block}>
+      <NavLink to={PATH.PACK_CARDS_PAGE}>
+        <ArrowBack className={style.arrow} />
+      </NavLink>
       <NavLink to={PATH.PACK_CARDS_PAGE} className={style.link}>
         Back to Packs List
       </NavLink>
