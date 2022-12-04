395a55e0fcba4ead1da0c571fdb8827176afdcc2
Cherniakov_Pavel <74683152+Pavel5284@users.noreply.github.com>
2022-12-05 00:26:16 +0200

    style: PageNotFound(404)

--- src/common/components/Page-404/PageNotFound.tsx
+++ src/common/components/Page-404/PageNotFound.tsx
@@ -2,20 +2,23 @@ import React from 'react'
 
 import { NavLink } from 'react-router-dom'
 
+import back from '../../../assets/img/404/404.png'
+import { SuperButton } from '../SuperButton/SuperButton'
 import { Wrapper } from '../Wrapper/Wrapper'
 
-import style from './NotFound404.module.css'
+import style from './NotFound404.module.scss'
 
 export const PageNotFound = () => {
   return (
     <Wrapper className={style.block}>
       <div className={style.content}>
-        <h1 className={style.title}>Page not Found</h1>
-        <p className={style.description}>Something Wrong</p>
-        <NavLink to={'/'} className={style.btn}>
-          ComeBack
+        <h1 className={style.title}>Ooops!</h1>
+        <p className={style.description}>Sorry! Page not found!</p>
+        <NavLink to={'/'} className={style.btn__link}>
+          <SuperButton title={'Back to home page'} className={style.btn} />
         </NavLink>
       </div>
+      <img src={back} alt="background" />
     </Wrapper>
   )
 }
